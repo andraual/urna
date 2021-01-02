8b96aea13e496fc192ea9079d98c835ff277499d
andraual <andraual@hotmail.com>
2021-01-02 00:17:41 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero:'50000',
+                nome: 'Tim Maia',
+                partido: 'Partido Socialista (PSOL)',
+                fotos:[
+                    {url:'v1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'13000',
+                nome: 'Voldemort',
+                partido: 'Partido dos Trabalhadores (PT)',
+                fotos:[
+                    {url:'v2.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero:'13',
+                nome: 'Batman',
+                partido: 'Partido Vampirico Comunista (PVC)',
+                vice: 'Robin',
+                fotos:[
+                    {url:'p1.png', legenda: 'Prefeito'},
+                    {url:'p1_1.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero:'50',
+                nome: 'Superman',
+                partido: 'Partido Jornalistico Liberal (PJL)',
+                Vice: 'Flash',
+                fotos:[
+                    {url:'p2.jpg', legenda: 'Vereador'},
+                    {url:'p2_1.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            }
+        ]
+    }
+];
